4c41b36a5b27799a7f0396a0dbd8903cc84e4045
alexeydementiev <adementiev@flyxo.com>
2022-05-15 20:02:04 +0300

    first commit

---
+++ src/main/java/classes/Person.java
@@ -0,0 +1,63 @@
+package classes;
+
+import java.time.Period;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+	protected String name;
+	protected String surname;
+	protected int age;
+	protected String currentCity;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setAge(int age) {
+		if (hasAge()) {
+			System.out.println("Произведена попытка изменить возраст. Менять заданный ранее возраст можно только при помощи процедуры happyBirthday");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public void setCurrentCity(String currentCity) {
+		this.currentCity = currentCity;
+	}
+
+	public PersonBuilder newChildBuilder() {
+		PersonBuilder child = new PersonBuilder().setSurname(this.surname).setAddress(this.currentCity);
+		return child;
+	}
+
+	public boolean hasAge() {
+		return this.age > 0 ? true : false;
+	}
+
+	public boolean hasAddress() {
+		return this.currentCity == null ? false : true;
+	}
+
+	public void happyBirthday() {
+		this.age++;
+		System.out.println("С днем рождения! Возвраст изменен на 1 год");
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", surname='" + surname + '\'' +
+				", age=" + age +
+				", currentCity='" + currentCity + '\'' +
+				'}';
+	}
+
+}
---
+++ src/main/java/classes/PersonBuilder.java
@@ -0,0 +1,47 @@
+package classes;
+
+public class PersonBuilder {
+
+	protected String name;
+	protected String surname;
+	protected int age;
+	protected String address;
+
+
+	public PersonBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+	public PersonBuilder setSurname(String surname) {
+		this.surname = surname;
+		return this;
+		}
+	public PersonBuilder setAge(int age) {
+		this.age = age;
+		return this;
+	}
+	public PersonBuilder setAddress(String address) {
+		this.address = address;
+		return this;
+		}
+
+	public Person build() {
+
+		if (name == null || name.equals("")) {
+			throw new IllegalStateException("Ошибка построения объекта типа Person: не указано имя");
+		}
+		if (surname == null || surname.equals("")) {
+			throw new IllegalStateException("Ошибка построения объекта типа Person: не указана фамилия");
+		}
+
+		if (age < 0) {
+			throw new IllegalStateException("Ошибка построения объекта типа Person: недопустимый возвраст");
+		}
+		Person person = new Person();
+		person.setName(name);
+		person.setSurname(surname);
+		person.setAge(age);
+		person.setCurrentCity(address);
+		return person;
+	}
+}
